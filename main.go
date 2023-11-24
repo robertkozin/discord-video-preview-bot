2dbc7efceea4ac1228c4a39949228d8b9724802c
Robert Kozin <robert@robertkozin.com>
2023-11-23 23:05:41 -0500

    Fix Spotify?

--- main.go
+++ main.go
@@ -257,13 +257,14 @@ func downloadSpotify(filename string, id string) (string, error) {
 	path := filepath.Join(previewDir, filename+".mp4")
 
 	audio := spvEndpoint + id + "/audio"
-	cmd := exec.Command("ffmpeg", "-f", "concat", "-safe", "0", "-protocol_whitelist", "file,https,tcp,tls,pipe,fd", "-i", "-", "-i", audio, "-vsync", "vfr", "-pix_fmt", "yuv420p", "-y", "-loglevel", "warning", path)
+	cmd := exec.Command("ffmpeg", "-f", "concat", "-safe", "0", "-protocol_whitelist", "file,https,tcp,tls,pipe,fd", "-i", "-", "-i", audio, "-c:v libx264 -tune stillimage -preset ultrafast -c:a aac -vf format=yuv420p -r 25 -movflags faststart -y", path)
 
 	in := bytes.Buffer{}
 	in.WriteString("file '")
 	in.WriteString(spvEndpoint)
 	in.WriteString(id)
 	in.WriteString("'\n")
+	in.WriteString("duration 30\n")
 	out := bytes.Buffer{}
 
 	cmd.Stdin = &in
