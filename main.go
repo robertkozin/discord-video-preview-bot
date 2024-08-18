f6bdc1b2e51a893e0f3531aa2d49c56f9deba082
Robert Kozin <robert@robertkozin.com>
2024-08-18 19:43:48 -0400

    Use new cobalt endpoint

--- main.go
+++ main.go
@@ -40,8 +40,8 @@ var history = make(map[string]string)
 var inFlightMessages = make(map[string]*discordgo.Message)
 var channelLastMessage = make(map[string]string)
 
-var cobaltEndpoint = "https://co.wuk.sh/api/json"    // https://github.com/wukko/cobalt
-var spvEndpoint = "https://spv.ncp.nathanferns.xyz/" // https://github.com/nathanielfernandes/spv
+var cobaltEndpoint = "https://api.cobalt.tools/api/json" // https://github.com/wukko/cobalt
+var spvEndpoint = "https://spv.ncp.nathanferns.xyz/"     // https://github.com/nathanielfernandes/spv
 
 type CobaltRequest struct {
 	Url             string `json:"url"`
