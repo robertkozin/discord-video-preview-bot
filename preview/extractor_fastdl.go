947eff9c6a49f42113dbfc95727c2ea390ae53fa
Robert Kozin <robert@robertkozin.com>
2025-09-10 09:26:41 +0200

    code support for fastdl extractor

---
+++ preview/extractor_fastdl.go
@@ -0,0 +1,56 @@
+package preview
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+)
+
+var _ Extractor = (*FastDLExtractor)(nil)
+
+type FastDLExtractor struct {
+	Endpoint string
+}
+
+func NewFastDL(config *url.URL) (*FastDLExtractor, error) {
+	endpoint := *config
+	endpoint.Scheme = "http"
+	return &FastDLExtractor{
+		Endpoint: endpoint.String(),
+	}, nil
+}
+
+func (fdl *FastDLExtractor) IsSupported(mediaURL string) bool {
+	return simpleURLMatch(mediaURL, []string{
+		"instagram.com/reel/*",
+		"instagram.com/p/*",
+		"instagram.com/story/*",
+	})
+}
+
+type VidProxyRequest struct {
+	Url string `json:"url"`
+}
+
+type VidProxyResponse struct {
+	RemoteURLs []string `json:"remote_urls"`
+}
+
+type VidProxyError struct {
+	Message string `json:"msg"`
+}
+
+func (vpe VidProxyError) Error() string {
+	return vpe.Message
+}
+
+func (fdl *FastDLExtractor) Extract(ctx context.Context, mediaURL string) ([]string, error) {
+	ctx, span := tracer.Start(ctx, "fastdl_extract")
+	defer span.End()
+
+	_, value, err := JSONRequest[VidProxyResponse, VidProxyError](ctx, "POST", fdl.Endpoint, VidProxyRequest{mediaURL})
+	if err != nil {
+		return nil, fmt.Errorf("making fastdl request: %w", err)
+	}
+	return value.RemoteURLs, nil
+}
